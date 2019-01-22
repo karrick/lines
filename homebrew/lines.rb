7969300bad36a270c3c97ab3c1a54f335eac8817
Karrick S. McDermott <karrick@karrick.net>
2019-01-22 16:05:05 -0500

    v0.5.2

--- homebrew/lines.rb
+++ homebrew/lines.rb
@@ -2,18 +2,18 @@ require 'rbconfig'
 class Lines < Formula
   desc "Print a range of lines from standard input or one or more files."
   homepage "https://github.com/karrick/lines"
-  version "0.5.1"
+  version "0.5.2"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/karrick/lines/releases/download/v0.5.1/lines_v0.5.1_darwin_amd64.zip"
-      sha256 "311b3cd88f62e38a5d98ef5e5827565ebd650775db2e206b7f2d3ed2580d368e"
+      url "https://github.com/karrick/lines/releases/download/v0.5.2/lines_0.5.2_darwin_amd64.zip"
+      sha256 "4e144cfabfb6cc564719aa8d3615abdb35e0ac8dbc0890be8c55122054db2347"
     when /linux/
-      url "https://github.com/karrick/lines/releases/download/v0.5.1/lines_v0.5.1_linux_amd64.tar.gz"
-      sha256 "b3673004e74359c42a66ba72ea2bea86aa9e6428c8ac399cd047fc4ca46fe622"
+      url "https://github.com/karrick/lines/releases/download/v0.5.2/lines_0.5.2_linux_amd64.tar.gz"
+      sha256 "e7e069e288875f69be394cdba66104780b90c72aa8993af37609d43261745859"
     when /solaris|bsd/
       :unix
     else
@@ -24,11 +24,11 @@ class Lines < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/karrick/lines/releases/download/v0.5.1/lines_v0.5.1_darwin_386.zip"
-      sha256 "6bbdc3986184fe277e2a06214e52cfb81db7f498a093d5b174f342abc99c2750"
+      url "https://github.com/karrick/lines/releases/download/v0.5.2/lines_0.5.2_darwin_386.zip"
+      sha256 "f78f84fec4e84d26a6c9b49bdf9b93bcab35b8f1b9b576ef4f5e2f6eff1fc18c"
     when /linux/
-      url "https://github.com/karrick/lines/releases/download/v0.5.1/lines_v0.5.1_linux_386.tar.gz"
-      sha256 "86baf20621821c1e3cc65ea8ec3cc6af4198be730d77880af2632e5741c7dfb3"
+      url "https://github.com/karrick/lines/releases/download/v0.5.2/lines_0.5.2_linux_386.tar.gz"
+      sha256 "78cec8ed8ae750821ff4c4a4032a097a3210cfcff6a782c9c89fe014bbd9e06e"
     when /solaris|bsd/
       :unix
     else
